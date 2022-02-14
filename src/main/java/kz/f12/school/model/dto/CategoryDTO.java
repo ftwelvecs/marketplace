ccd7ab1e8f5e4eeabdb335dd1bd02ceb3fe7daf3
Toktar <ufazak@gmail.com>
2022-02-15 02:05:11 +0600

    Реализовываем mapper для json объектов

--- src/main/java/kz/f12/school/model/dto/CategoryDTO.java
+++ src/main/java/kz/f12/school/model/dto/CategoryDTO.java
@@ -1,15 +1,15 @@
 package kz.f12.school.model.dto;
 
 public class CategoryDTO {
-    private int id;
+    private Integer id;
     private String name;
     private String description;
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
--- src/main/java/kz/f12/school/util/Mapper.java
+++ src/main/java/kz/f12/school/util/Mapper.java
@@ -3,6 +3,7 @@ package kz.f12.school.util;
 import kz.f12.school.model.dto.CategoryDTO;
 import kz.f12.school.model.dto.ProductDTO;
 import kz.f12.school.model.repository.CategoryRepository;
+import org.json.JSONObject;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -38,6 +39,18 @@ public class Mapper {
         return productDTO;
     }
 
+    public static ProductDTO toProductDTO(JSONObject json) {
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setId(!json.isNull("id") ? json.getInt("id") : null);
+        productDTO.setName(!json.isNull("name") ? json.getString("name") : null);
+        productDTO.setDescription(!json.isNull("description") ? json.getString("description") : null);
+        productDTO.setQuantity(!json.isNull("quantity") ? json.getInt("quantity") : null);
+        productDTO.setPrice(!json.isNull("price") ? json.getDouble("price") : null);
+        productDTO.setWeight(!json.isNull("weight") ? json.getDouble("weight") : null);
+        productDTO.setCategoryDTO(!json.isNull("category") ? toCategoryDTO(json.getJSONObject("category")) : null);
+        return productDTO;
+    }
+
     public static CategoryDTO toCategoryDTO(ResultSet resultSet) {
         // создаем объект продукта
         // shift + f6 <- быстро поменять все именования переменной
@@ -55,6 +68,14 @@ public class Mapper {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return categoryDTO;
+    }
+
+    public static CategoryDTO toCategoryDTO(JSONObject json) {
+        CategoryDTO categoryDTO = new CategoryDTO();
+        categoryDTO.setId(!json.isNull("id") ? json.getInt("id") : null);
+        categoryDTO.setName(!json.isNull("name") ? json.getString("name") : null);
+        categoryDTO.setDescription(!json.isNull("description") ? json.getString("description") : null);
         return categoryDTO;
     }
 

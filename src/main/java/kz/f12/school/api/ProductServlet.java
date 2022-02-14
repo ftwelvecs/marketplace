32531ed9e0dc838674e6aa8c2f154abe78fbc621
Toktar <ufazak@gmail.com>
2022-02-15 01:52:59 +0600

    В productServlet реализовали метод добавления

--- src/main/java/kz/f12/school/api/ProductServlet.java
+++ src/main/java/kz/f12/school/api/ProductServlet.java
@@ -2,6 +2,7 @@ package kz.f12.school.api;
 
 import kz.f12.school.model.dto.ProductDTO;
 import kz.f12.school.service.ProductService;
+import kz.f12.school.util.Mapper;
 import kz.f12.school.util.ServletUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -14,6 +15,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
+import static kz.f12.school.util.ServletUtils.getJsonBody;
+
 @WebServlet("/products/*")
 public class ProductServlet extends HttpServlet {
 
@@ -21,18 +24,25 @@ public class ProductServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        try {
-            String methodName = req.getPathInfo().split("/")[1];
-            if ("getById".equals(methodName)) {
-                int id = Integer.parseInt(req.getPathInfo().split("/")[2]);
-                ProductDTO productDTO = productService.getById(id);
-                JSONObject jsonObject = new JSONObject(productDTO);
+        String methodName = req.getPathInfo().split("/")[1];
+        if ("getById".equals(methodName)) {
+            int id = Integer.parseInt(req.getPathInfo().split("/")[2]);
+            ProductDTO productDTO = productService.getById(id);
+            JSONObject jsonObject = new JSONObject(productDTO);
 
-                ServletUtils.printContent(jsonObject.toString(), ServletUtils.JSON_TYPE, resp);
-            } else {
-                showDefaultContent(resp);
-            }
-        } catch (Exception e) {
+            ServletUtils.printContent(jsonObject.toString(), ServletUtils.JSON_TYPE, resp);
+        } else {
+            showDefaultContent(resp);
+        }
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String methodName = req.getPathInfo().split("/")[1];
+        if ("create".equals(methodName)) {
+            ProductDTO productDTO = Mapper.toProductDTO(getJsonBody(req));
+            productService.save(productDTO);
+        } else {
             showDefaultContent(resp);
         }
     }
